245153185a73be3dfe8b482b4066f627acf1bb89
Lennart K <lekahade@gmail.com>
2019-03-14 22:21:11 +0100

    Added Szenes
    You can now collect a series of states and actions in a szene to invoke it

---
+++ homecontrol/modules/szenes/module.py
@@ -0,0 +1,41 @@
+class Module:
+    async def init(self):
+        data = self.core.cfg.get("szenes", [])
+        self.szenes = {szene["alias"]: Szene(self, szene) for szene in data}
+
+        @event("gather_automation_providers")
+        async def on_gather_automation_providers(event, engine, callback):
+            callback(action={"szene": self.provider_factory})
+
+    def provider_factory(self, rule, engine):
+        target = rule.data["action"]["target"]
+        return self.szenes[target]
+
+
+class Szene:
+    def __init__(self, module, data):
+        self.core = module.core
+        self.data = data
+    
+    async def invoke(self):
+        for item_id, data in self.data.get("items", {}).items():
+            item = self.core.entity_manager.items[item_id]
+
+            for state_name, value in data.get("state", {}).items():
+                await item.states.set(state_name, value)
+
+            for action_instruction in data.get("action", []):
+                await item.actions.execute(action_instruction["name"], **action_instruction.get("data", {}))
+
+    async def on_trigger(self, data):
+        return await self.invoke()
+
+class SzeneActionProvider:
+    def __init__(self, module, rule, engine):
+        self.rule = rule
+        self.module = module
+        self.engine = engine
+        self.core = engine.core
+
+    async def on_trigger(self, data):
+        return await self.module.szenes[self.rule["trigger"]["target"]].invoke()

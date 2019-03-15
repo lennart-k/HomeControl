6f69e485047985f8b021554c7a1155b78243e363
Lennart K <lekahade@gmail.com>
2019-03-15 17:58:43 +0100

    Implemented one-file modules
    Modules don't have to consist of a whole folder anymore.

--- homecontrol/dependencies/module_manager.py
+++ homecontrol/dependencies/module_manager.py
@@ -16,13 +16,56 @@ class ModuleManager:
         Loads every module in a folder
         """
         out = []
-        for folder in os.listdir(path):
-            if os.path.isdir(os.path.join(path, folder)):
-                out.append(await self.load_module(os.path.join(path, folder), folder))
+        for node in os.listdir(path):
+            mod_path = os.path.join(path, node)
+
+            if os.path.isdir(mod_path):
+                out.append(await self.load_module(mod_path, node))
+
+            elif os.path.isfile(mod_path) and node.endswith(".py"):
+                out.append(await self.load_file_module(mod_path, ".".join(os.path.splitext(node)[:-1])))
 
         return out
 
-    async def load_module(self, path, name) -> (object, Exception):
+    async def load_file_module(self, mod_path, name) -> (Module, Exception):
+        """
+        Loads a module from a file and send an init trigger
+        """
+        try:
+            assert os.path.isfile(mod_path)
+        except AssertionError as e:
+            self.core.event_engine.broadcast("module_not_loaded", exception=e)
+            return e
+
+        spec = importlib.util.spec_from_file_location(name, mod_path)
+        mod = importlib.util.module_from_spec(spec)
+        mod.event = self.core.event_engine.register
+        mod.tick = self.core.tick_engine.tick
+        spec.loader.exec_module(mod)
+        if not hasattr(mod, "Module"):
+            mod.Module = Module
+
+        cfg = (mod.SPEC if type(mod.SPEC) == dict else yaml.load(mod.SPEC)) if hasattr(mod, "SPEC") else {}
+
+        mod_obj = mod.Module.__new__(mod.Module)
+        mod_obj.core = self.core
+        mod_obj.meta = cfg.get("meta", {})
+        mod_obj.name = name
+        mod_obj.path = mod_path
+        mod_obj.items = {}
+        mod_obj.item_specs = {}
+        mod_obj.mod = mod
+        mod_obj.spec = cfg
+        mod_obj.__init__()
+
+        self.loaded_modules[name] = mod_obj
+        await self.core.entity_manager.add_from_module(mod_obj)
+        if hasattr(mod_obj, "init"):
+            await mod_obj.init()
+        return mod_obj
+
+
+    async def load_module(self, path, name) -> (Module, Exception):
         """
         Loads a module from a folder and send an init trigger
         """
@@ -51,9 +94,7 @@ class ModuleManager:
         mod_obj.meta = cfg.get("meta", {})
         mod_obj.name = name
         mod_obj.path = path
-        mod_obj.adapters = {}
         mod_obj.items = {}
-        mod_obj.adapter_specs = {}
         mod_obj.item_specs = {}
         mod_obj.mod = mod
         mod_obj.spec = cfg
@@ -63,7 +104,6 @@ class ModuleManager:
         await self.core.entity_manager.add_from_module(mod_obj)
         if hasattr(mod_obj, "init"):
             await mod_obj.init()
-        # self.core.event_engine.trigger("init", mod_obj)
         return mod_obj
 
     async def unload_module(self, name) -> None:

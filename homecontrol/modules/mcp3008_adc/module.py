c63c4d18aaaed0842aa884fab9235942f93b7607
lennart-k <lekahade@gmail.com>
2019-03-28 11:47:24 +0100

    Added support for the MCP3008 ADC
    This enables the Raspberry Pi to get analog input

---
+++ homecontrol/modules/mcp3008_adc/module.py
@@ -0,0 +1,43 @@
+import pigpio
+
+
+class MCP3008ADC:
+    handle = None
+
+    async def init(self) -> bool:
+        self.handle = self.cfg["pigpio_adapter"].pigpio.spi_open(
+            spi_channel=self.cfg["spi_channel"],
+            baud=self.cfg["baud_rate"],
+            spi_flags=self.cfg["spi_flags"]
+        )
+
+    def get_value(self, channel: int) -> int:
+        adc = self.cfg["pigpio_adapter"].pigpio.spi_xfer(self.handle, [1, (8 + channel) << 4, 0])[1]
+        return ((adc[1] & 3) << 8) + adc[2]
+
+
+    async def stop(self):
+        if not self.handle == None:
+            self.cfg["pigpio_adapter"].pigpio.spi_close(self.handle)
+
+
+class AnalogInput:
+    async def init(self):
+        self.adc = self.cfg["adc"]
+        self.raw_val = 0
+        tick(self.cfg["update_interval"])(self.poll_value)
+
+    async def get_value(self):
+        return self.adc.get_value(self.cfg["channel"])
+
+    async def poll_value(self):
+        new_raw_val = await self.get_value()
+        if abs(self.raw_val - new_raw_val) >= self.cfg["change_threshold"]:
+            await self.update_raw(new_raw_val)
+
+    async def update_raw(self, raw_val):
+        self.raw_val = raw_val
+        await self.states.update("value", self.value(raw_val))
+
+    def value(self, raw_val) -> int:
+        return int(self.cfg["min"]+raw_val*(self.cfg["max"]-self.cfg["min"])/1023+0.5)

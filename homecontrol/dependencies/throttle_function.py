b4927f45766b05ce7bbb1e4b40d6af7e54a08df2
lennart-k <lekahade@gmail.com>
2019-03-30 14:12:59 +0100

    Code optimisation

--- homecontrol/dependencies/throttle_function.py
+++ homecontrol/dependencies/throttle_function.py
@@ -3,17 +3,19 @@ import time
 
 
 class throttle(object):
-    def __init__(self, s=1):
+    """
+    Throttles a function so it only gets called at a fixed frequency
+    """
+    def __init__(self, s: float = 1):
         self.throttle_period = s
         self.time_of_last_call = time.time()
 
-    def __call__(self, fn):
+    def __call__(self, fn: callable):
         @wraps(fn)
         def wrapper(*args, **kwargs):
             now = time.time()
-            time_since_last_call = now - self.time_of_last_call
 
-            if time_since_last_call > self.throttle_period:
+            if now - self.time_of_last_call > self.throttle_period:
                 self.time_of_last_call = now
                 return fn(*args, **kwargs)
 

608326dc1269a757a0061b76ff96e67358dae84c
lennart-k <lekahade@gmail.com>
2019-04-06 12:21:09 +0200

    Made rgb the default color format for initialisation

--- homecontrol/dependencies/data_types.py
+++ homecontrol/dependencies/data_types.py
@@ -18,8 +18,9 @@ def data_type(cls: object) -> object:
 class Color:
     hsl: (int, int, int)
 
-    def __init__(self, h: int, s: int, l: int):
-        self.hsl = (h, s, l)
+    def __init__(self, r: int, g: int, b: int):
+        hls = colorsys.rgb_to_hls(*(i/255 for i in (r, g, b)))
+        self.hsl = int(hls[0]*360), int(hls[2]*255), int(hls[1]*255)
 
     @staticmethod
     def from_hsl(hsl: tuple):

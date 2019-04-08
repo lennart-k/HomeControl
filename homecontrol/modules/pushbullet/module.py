925c5fba5d060348d835873496b0768d84171db4
lennart-k <lekahade@gmail.com>
2019-04-08 11:55:23 +0200

    Code optimisation

--- homecontrol/modules/pushbullet/module.py
+++ homecontrol/modules/pushbullet/module.py
@@ -8,7 +8,7 @@ ME_URL = BASE_URL+"/users/me"
 
 class Pushbullet:
     async def init(self):
-         working = requests.get(ME_URL, headers={
+         return requests.get(ME_URL, headers={
             "Access-Token": self.cfg["access_token"],
             "Content-Type": "application/json"
         }).status_code == 200
@@ -18,7 +18,7 @@ class Pushbullet:
             "type": "note",
             **data
         }
-        result = requests.post(PUSH_URL, json.dumps(data), headers={
+        requests.post(PUSH_URL, json.dumps(data), headers={
             "Access-Token": self.cfg["access_token"],
             "Content-Type": "application/json"
-        }).json()
+        })

3da11cbd6dc10e418be2ba7cd0cb9940ac943383
lennart-k <lekahade@gmail.com>
2019-04-20 16:43:03 +0200

    tasmota_rf_adapter: Added a TX interval
    Otherwise it may happen that HomeControl tries to send two codes at once,
    resulting a loss

--- homecontrol/modules/tasmota_rf_adapter/module.py
+++ homecontrol/modules/tasmota_rf_adapter/module.py
@@ -1,10 +1,16 @@
 from contextlib import suppress
 import json
 from functools import reduce
+import asyncio
 
 
 class TasmotaRFAdapter:
+    sending: asyncio.Event
+
     async def init(self):
+        self.sending = asyncio.Event()
+        self.sending.set()
+
         @event("mqtt_connected")
         async def on_mqtt_connected(event, mqtt_adapter):
             if mqtt_adapter == self.cfg["mqtt_adapter"]:
@@ -20,10 +26,17 @@ class TasmotaRFAdapter:
                         bits = bin(int(code, 16))[2:][::2]
                         self.core.event_engine.broadcast("rf_code_received", code=int(bits, 2), length=len(bits))
 
-    async def send_code(self, code):
+    async def action_send_code(self, code) -> None:
+        await self.sending.wait()
+        self.sending.clear()
         binary = bin(int(code))[2:]
         data = "#"+hex(int("".join(reduce(lambda x, y: x+y, zip(["0"]*len(binary), binary))), 2))[2:]
         self.cfg["mqtt_adapter"].client.publish(self.cfg["topic"]+"/cmnd/RfCode", data)
+        self.core.loop.call_later(self.cfg["tx_interval"], self.sending.set)
+
+    async def send_code(self, code: int) -> None:
+        asyncio.ensure_future(self.action_send_code(code))
+
 
     async def stop(self):
         self.cfg["mqtt_adapter"].client.unsubscribe(self.cfg["topic"]+"/tele/RESULT")

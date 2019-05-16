47a0e651bb8a2e8c088866bce2db6892f5262cfe
Lennart K <lekahade@gmail.com>
2019-05-16 23:34:11 +0200

    Basic support for websockets

--- homecontrol/modules/api/module.py
+++ homecontrol/modules/api/module.py
@@ -1,9 +1,13 @@
+"""The API for HomeControl"""
+
 import logging
 from collections import ChainMap
-import voluptuous as vol
-from homecontrol.dependencies import json
 from json import JSONDecodeError
+
+import voluptuous as vol
 from aiohttp import web
+
+from homecontrol.dependencies import json
 from homecontrol.dependencies.json_response import JSONResponse
 from homecontrol.const import (
     ERROR_ITEM_NOT_FOUND,
@@ -15,10 +19,17 @@ from homecontrol.const import (
 )
 
 LOGGER = logging.getLogger(__name__)
+
+
 class Module:
+    """The API app module"""
     api_app: web.Application
 
     async def init(self):
+        """Initialise the API app"""
+        self.event_sockets = set()
+        self.api_app = None
+
         @event("http_add_main_subapps")
         async def add_subapp(event, main_app):
             middlewares = self.middlewares()
@@ -30,6 +41,7 @@ class Module:
             main_app.add_subapp("/api", self.api_app)
 
     def middlewares(self) -> list:
+        """Return middlewares"""
         middlewares = []
 
         @middlewares.append
@@ -43,10 +55,15 @@ class Module:
         return middlewares
 
     async def stop(self):
-        if self.api_app.frozen:
+        """Stop the API app"""
+        for websocket in list(self.event_sockets):
+            await websocket.close()
+        if self.api_app and self.api_app.frozen:
             await self.api_app.cleanup()
 
     def routes(self) -> web.RouteTableDef:
+        """Return the API routes"""
+        # pylint: disable=invalid-name,too-many-locals,too-many-statements
         r = web.RouteTableDef()
 
         @event("state_change")
@@ -59,15 +76,41 @@ class Module:
                         "changes": changes
                     }, dumps=json.dumps)
 
+                # pylint: disable=broad-except
                 except Exception as e:
-                    print(e.__traceback__)
+                    LOGGER.debug(
+                        "An error occured when sending a state update over WebSocket",
+                        exc_info=True)
+
+        @r.get("/websocket")
+        async def events_websockets(request: web.Request) -> web.WebSocketResponse:
+            """The WebSocket route"""
+            websocket = web.WebSocketResponse()
+            await websocket.prepare(request)
+            self.event_sockets.add(websocket)
+
+            async for msg in websocket:
+                try:
+                    data = json.loads(msg.data)
+                # pylint: disable=broad-except
+                except Exception:
+                    continue
+
+                if msg.data == "close":
+                    await websocket.close()
+
+            self.event_sockets.remove(websocket)
+            return websocket
+
 
         @r.get("/ping")
         async def ping(request: web.Request) -> JSONResponse:
+            """Handle /ping"""
             return JSONResponse("PONG")
 
         @r.post("/core/shutdown")
         async def shutdown(request: web.Request) -> JSONResponse:
+            """Handle /core/shutdown"""
             self.core.loop.create_task(self.core.shutdown())
             return JSONResponse("Shutting down")
 
@@ -141,19 +184,22 @@ class Module:
                 content = (await request.content.read()).decode()
                 commit = STATE_COMMIT_SCHEMA(
                     json.loads(content) if content else {})
+            # pylint: disable=broad-except
             except Exception as e:
                 return JSONResponse(error=e)
 
             if not commit.keys() & item.states.states.keys() == commit.keys():
                 return JSONResponse(error={
                     "type": ERROR_INVALID_ITEM_STATES,
-                    "message": f"The given states {set(commit.keys())} do not comply with accepted states {set(item.states.states.keys())}"
+                    "message": f"The given states {set(commit.keys())} do not comply"
+                               f"with accepted states {set(item.states.states.keys())}"
                 })
 
-            return JSONResponse(dict(ChainMap(*[await item.states.set(state, value) for state, value in commit.items()])))
+            return JSONResponse(dict(ChainMap(
+                *[await item.states.set(state, value) for state, value in commit.items()])))
 
         @r.post("/item/{id}/states/{state_name}")
-        async def set_item_states(request: web.Request) -> JSONResponse:
+        async def set_item_state(request: web.Request) -> JSONResponse:
             identifier = request.match_info["id"]
             item = self.core.item_manager.items.get(identifier)
 
@@ -175,6 +221,7 @@ class Module:
                 content = (await request.content.read()).decode()
                 value = json.loads(content) if content else {}
                 result = await item.states.set(state_name, value)
+            # pylint: disable=broad-except
             except (Exception, vol.error.Error) as e:
                 return JSONResponse(error=e)
 
@@ -223,7 +270,7 @@ class Module:
         async def execute_action(request: web.Request) -> JSONResponse:
             """
             Executes an item's action and returns a boolean indicating the success of the action.
-            Changed states or other events can be handled through the websocket endpoint /websocket
+            Changed states or other events can be handled through the websocket endpoint /events
             """
 
             identifier = request.match_info["id"]
@@ -244,7 +291,8 @@ class Module:
             if action_name not in item.actions.actions:
                 return JSONResponse(error={
                     "type": ITEM_ACTION_NOT_FOUND,
-                    "message": f"Item {identifier} of type {item.type} does not have an action {action_name}"
+                    "message": f"Item {identifier} of type"
+                               f"{item.type} does not have an action {action_name}"
                 })
 
             try:
@@ -253,6 +301,7 @@ class Module:
                     "action": action_name,
                     "result": await item.actions.execute(action_name, **kwargs)
                 })
+            # pylint: disable=broad-except
             except Exception as e:
                 return JSONResponse(error=e)
 

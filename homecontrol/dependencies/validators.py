76f7924881586dc716f50f040f9ffc243c5c8e3a
lennart-k <lekahade@gmail.com>
2019-05-17 17:39:16 +0200

    Added a new validator

---
+++ homecontrol/dependencies/validators.py
@@ -0,0 +1,26 @@
+"""Custom validators for voluptuous"""
+
+import voluptuous as vol
+
+
+class ConsistsOf(object):
+    """Validate that the given schema element applies to every element in a sequence.
+
+    >>> s = ConsistsOf({"a": int})
+    >>> s([{"a": 1}, {"a": 2}, {"a": 3}])
+    [{"a": 1}, {"a": 2}, {"a": 3}]
+    """
+
+    def __init__(self, schema, msg=None):
+        self.schema = vol.Schema(schema)
+        self.msg = msg
+
+    # pylint: disable=invalid-name
+    def __call__(self, v):
+        if not hasattr(v, "__iter__"):
+            return vol.Invalid('value is not iterable')
+
+        return [self.schema(element) for element in v]
+
+    def __repr__(self):
+        return 'ConsistsOf(%s)' % (self.schema,)

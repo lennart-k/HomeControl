fc0c55830b7ee8deb9e72c8dc296833a2920dced
lennart-k <lekahade@gmail.com>
2019-05-20 12:04:32 +0200

    Fixed mqtt_adapter

--- homecontrol/modules/mqtt_adapter/module.py
+++ homecontrol/modules/mqtt_adapter/module.py
@@ -15,6 +15,7 @@ class MQTTAdapter:
         self.client.connect_async(self.cfg["host"], self.cfg["port"])
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
+        self.client.on_disconnect = self.on_disconnect
         self.client.loop_start()
         try:
             await asyncio.wait_for(self.connected.wait(), 3)
@@ -23,13 +24,18 @@ class MQTTAdapter:
 
     async def stop(self):
         """Stop the mqtt session"""
-        self.client.disconnect()
-        self.client.loop_stop(True)
+        if self.connected:
+            self.client.disconnect()
+        self.core.loop.call_soon(self.core.loop.run_in_executor(None, self.client.loop_stop))
 
     def on_connect(self, _, userdata, flags, result):
         """Handle a connection"""
         self.connected.set()
         self.core.event_engine.broadcast_threaded("mqtt_connected", mqtt_adapter=self)
+
+    def on_disconnect(self, _, userdata, mid) -> None:
+        """Handle on_disconnect"""
+        self.connected.clear()
 
     def on_message(self, _, userdata, msg):
         """Handle a message"""

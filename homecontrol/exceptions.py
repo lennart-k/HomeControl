4f86c7f2ff90f403a2c398f0ce3a6f822fea5528
lennart-k <lekahade@gmail.com>
2019-06-07 12:20:18 +0200

    :fire: Removing useless exceptions

--- homecontrol/exceptions.py
+++ homecontrol/exceptions.py
@@ -13,23 +13,8 @@ class ModuleNotFoundException(HomeControlException):
     """Module not found"""
 
 
-class NoCoreException(HomeControlException):
-    """
-    No core passed. Can for example be raised by yaml_loader
-    when trying to construct an item
-    """
-
-
-class InvalidConfigException(HomeControlException):
-    """There's an error in the config directory"""
-
-
 class PipInstallError(HomeControlException):
     """A pip install failed"""
-
-
-class ItemTypeNotFound(HomeControlException):
-    """There is no spec for the item type"""
 
 
 class ConfigDomainAlreadyRegistered(HomeControlException):

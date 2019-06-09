15a8dda02d009caa664620af0944d0daa3c1d33b
lennart-k <lekahade@gmail.com>
2019-06-09 21:52:16 +0200

    :sparkles: Added a simple Speedtest module

---
+++ homecontrol/modules/speedtest/module.py
@@ -0,0 +1,35 @@
+"""Module for internet speedtest"""
+
+import logging
+
+import speedtest
+
+LOGGER = logging.getLogger(__name__)
+
+
+class Speedtest:
+    """The Speedtest item"""
+    async def init(self) -> None:
+        """Init the Speedtest item"""
+        tick(self.cfg["update_interval"])(self.measure)
+
+    async def measure(self) -> None:
+        """
+        Measures the internetspeed
+        Gets called by state polling
+        """
+        LOGGER.debug("Doing speedtest now")
+        s = speedtest.Speedtest()
+        s.get_best_server()
+        s.download()
+        s.upload()
+        await self.states.bulk_update(
+            ping=s.results.ping,
+            upload_speed=s.results.upload,
+            download_speed=s.results.download
+        )
+
+    async def stop(self) -> None:
+        """Stop the speedtest item"""
+        self.core.tick_engine.remove_tick(
+            self.cfg["update_interval"], self.measure)

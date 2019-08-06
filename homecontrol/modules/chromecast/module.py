537f8a06320e69b14cd3e66427ce2d1ece66cf7f
lennart-k <18233294+lennart-k@users.noreply.github.com>
2019-08-06 21:33:45 +0200

    :art: chromecast: Added type hints

--- homecontrol/modules/chromecast/module.py
+++ homecontrol/modules/chromecast/module.py
@@ -7,7 +7,7 @@ class Chromecast:
     """The Chromecast item"""
     last_time_jump = 0
 
-    async def init(self):
+    async def init(self) -> None:
         """Initialises the Chromecast item"""
         try:
             self.chromecast = pychromecast.Chromecast(
@@ -27,33 +27,33 @@ class Chromecast:
         )
         self.media_controller.register_status_listener(self)
 
-    async def get_playtime(self):
+    async def get_playtime(self) -> float:
         """Getter for playtime"""
         await self.states.update(
             "playtime", vars(self.media_controller).get("current_time"))
         return vars(self.media_controller).get("current_time")
 
-    async def set_playtime(self, value):
+    async def set_playtime(self, value: float) -> dict:
         """"Setter for playtime"""
         value = max(0, min(value, await self.states.get("duration")))
         self.media_controller.seek(value)
         await self.states.update("playtime", value)
         return {"playtime": value}
 
-    async def action_pause(self):
+    async def action_pause(self) -> bool:
         """Action: Pause"""
         self.media_controller.pause()
         return True
 
-    async def get_active(self):
+    async def get_active(self) -> bool:
         """Get if Chromecast is active"""
         return self.chromecast.media_controller.is_active
 
-    async def get_playing(self):
+    async def get_playing(self) -> bool:
         """Getter for playing"""
         return self.chromecast.media_controller.is_playing
 
-    async def set_playing(self, value):
+    async def set_playing(self, value) -> dict:
         """Setter for playing"""
         if value:
             self.media_controller.play()
@@ -62,55 +62,52 @@ class Chromecast:
         await self.states.update("playing", value)
         return {"playing": value}
 
-    async def action_play(self):
+    async def action_play(self) -> bool:
         """Action: Play"""
         self.media_controller.play()
         return True
 
-    async def action_play_url(self, url, mime):
+    async def action_play_url(self, url: str, mime: str) -> None:
         """Action: Play URL"""
         self.media_controller.play_media(url=url, content_type=mime)
 
-    async def action_rewind(self):
+    async def action_rewind(self) -> None:
         """Action: Rewind"""
         self.media_controller.rewind()
-        return True
 
-    async def action_skip(self):
+    async def action_skip(self) -> None:
         """Action: Skip"""
         self.media_controller.skip()
-        return True
 
-    async def action_stop(self):
+    async def action_stop(self) -> None:
         """Action: stop"""
         if self.media_controller.is_active:
             self.media_controller.stop()
-        return True
 
-    async def set_volume(self, value):
+    async def set_volume(self, value: int) -> dict:
         """Setter for volume"""
         self.chromecast.set_volume(value / 100)
         return ({"volume": value}
                 if await self.states.update("volume", value)
                 else {})
 
-    async def set_muted(self, value):
+    async def set_muted(self, value: bool) -> dict:
         """Setter for muted"""
         self.chromecast.set_volume_muted(bool(value))
         return ({"muted": value}
                 if await self.states.update("muted", value)
                 else {})
 
-    async def action_toggle_muted(self):
+    async def action_toggle_muted(self) -> None:
         """Action: Toggle muted"""
         new_state = not await self.states.get("muted")
         await self.states.set("muted", new_state)
 
-    async def action_quit(self):
+    async def action_quit(self) -> None:
         """Action: Quit casting"""
         self.chromecast.quit_app()
 
-    def new_cast_status(self, status):
+    def new_cast_status(self, status) -> None:
         """Handle new cast status"""
         self.core.event_engine.broadcast(
             "chromecast_cast_status", status=status)

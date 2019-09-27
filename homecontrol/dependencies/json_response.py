4b6581f30e26474145181f8180b1f8454f4a94ad
lennart-k <18233294+lennart-k@users.noreply.github.com>
2019-09-27 22:01:05 +0200

    :recycle: json_response: Removed the old response format

--- homecontrol/dependencies/json_response.py
+++ homecontrol/dependencies/json_response.py
@@ -9,18 +9,16 @@ from homecontrol.dependencies import json
 class JSONResponse(web.Response):
     """A HTTP response for JSON data"""
 
-    def __init__(self,
-                 data: Any = None,
-                 error: Exception = None,
-                 status_code: int = 200,
-                 core=None,
-                 headers: dict = None):
-        response = {
-            "success": not error,
-            **({"error": error} if error else {}),
-            "status_code": status_code,
-            **({"data": data} if not error else {})
-        }
+    def __init__(
+            self,
+            data: Any = None,
+            error: str = None,
+            status_code: int = 200,
+            core=None,
+            headers: dict = None) -> None:
+
+        response = {"error": error} if error else data
+
         super().__init__(body=json.dumps(response, indent=4, sort_keys=True,
                                          core=core),
                          status=status_code, content_type="application/json",

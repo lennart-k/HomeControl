40cf6d6a9b5a96d91224908896c961e2e011af62
lennart-k <18233294+lennart-k@users.noreply.github.com>
2019-10-19 19:53:36 +0200

    :recycle: Rewrote ensure_pip_requirements properly

--- homecontrol/dependencies/ensure_pip_requirements.py
+++ homecontrol/dependencies/ensure_pip_requirements.py
@@ -2,44 +2,24 @@
 
 import sys
 import subprocess
-import json
 from typing import Set
-
-# pylint: disable=no-name-in-module,import-error
-from pip._vendor.distlib.version import NormalizedMatcher
+from pkg_resources import require, VersionConflict, DistributionNotFound
 
 from homecontrol.exceptions import PipInstallError
-
-INSTALLED_REQUIREMENTS = None
 
 
 def ensure_pip_requirements(requirements: Set[str]) -> None:
     """Ensure that pip requirements are installed"""
-
-    global INSTALLED_REQUIREMENTS  # pylint: disable=global-statement
-
-    if INSTALLED_REQUIREMENTS is None:
-        pip_list = subprocess.check_output(
-            [sys.executable, "-m", "pip", "list", "--format=json",
-             "--disable-pip-version-check"])
-
-        INSTALLED_REQUIREMENTS = {
-            item["name"]: item["version"] for item in json.loads(pip_list)}
-
-    unsatisfied_requirements = set()
-
+    unsatisfied = set()
     for requirement in requirements:
-        matcher = NormalizedMatcher(requirement)
-        if matcher.name not in INSTALLED_REQUIREMENTS:
-            unsatisfied_requirements.add(requirement)
-            continue
-        if not matcher.match(INSTALLED_REQUIREMENTS[matcher.name]):
-            unsatisfied_requirements.add(requirement)
+        try:
+            require(requirement)
+        except (DistributionNotFound, VersionConflict):
+            unsatisfied.add(requirement)
 
-    if unsatisfied_requirements:
+    if unsatisfied:
         process = subprocess.Popen([
-            sys.executable, "-m", "pip", "install", *unsatisfied_requirements
-        ])
+            sys.executable, "-m", "pip", "install", *unsatisfied])
         if process.wait():
             raise PipInstallError(
                 "An error occured when installing pip requirements")

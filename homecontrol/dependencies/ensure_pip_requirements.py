fec40cc001968a46cd05e1d22ad120ca0c9f5dab
lennart-k <18233294+lennart-k@users.noreply.github.com>
2019-10-19 19:02:14 +0200

    :zap: Fixed ensure_pip so that it doesn't trigger pip with every module install

--- homecontrol/dependencies/ensure_pip_requirements.py
+++ homecontrol/dependencies/ensure_pip_requirements.py
@@ -10,24 +10,30 @@ from pip._vendor.distlib.version import NormalizedMatcher
 
 from homecontrol.exceptions import PipInstallError
 
+INSTALLED_REQUIREMENTS = None
+
 
 def ensure_pip_requirements(requirements: Set[str]) -> None:
     """Ensure that pip requirements are installed"""
-    pip_list = subprocess.check_output(
-        [sys.executable, "-m", "pip", "list", "--format=json",
-         "--disable-pip-version-check"])
 
-    installed_requirements = {
-        item["name"]: item["version"] for item in json.loads(pip_list)}
+    global INSTALLED_REQUIREMENTS  # pylint: disable=global-statement
+
+    if INSTALLED_REQUIREMENTS is None:
+        pip_list = subprocess.check_output(
+            [sys.executable, "-m", "pip", "list", "--format=json",
+             "--disable-pip-version-check"])
+
+        INSTALLED_REQUIREMENTS = {
+            item["name"]: item["version"] for item in json.loads(pip_list)}
 
     unsatisfied_requirements = set()
 
     for requirement in requirements:
         matcher = NormalizedMatcher(requirement)
-        if matcher.name not in installed_requirements:
+        if matcher.name not in INSTALLED_REQUIREMENTS:
             unsatisfied_requirements.add(requirement)
             continue
-        if not matcher.match(installed_requirements[matcher.name]):
+        if not matcher.match(INSTALLED_REQUIREMENTS[matcher.name]):
             unsatisfied_requirements.add(requirement)
 
     if unsatisfied_requirements:

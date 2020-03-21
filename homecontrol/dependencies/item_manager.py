3d0a3a7ea88bf989ae02f40d3764964c74de3a38
lennart-k <18233294+lennart-k@users.noreply.github.com>
2020-03-21 16:25:16 +0100

    :sparkles: Adding a storage backend to item_manager
    This paves the way for item discovery and frontend-based configuration

--- homecontrol/dependencies/item_manager.py
+++ homecontrol/dependencies/item_manager.py
@@ -1,6 +1,4 @@
 """ItemManager for HomeControl"""
-
-import asyncio
 from inspect import isclass
 import logging
 import voluptuous as vol
@@ -12,6 +10,7 @@ from homecontrol.const import (
     EVENT_ITEM_NOT_WORKING
 )
 from homecontrol.dependencies.entity_types import Item, Module
+from homecontrol.dependencies.storage import Storage
 
 LOGGER = logging.getLogger(__name__)
 
@@ -20,9 +19,31 @@ CONFIG_SCHEMA = vol.Schema([
         vol.Required("id"): str,
         vol.Required("type"): str,
         vol.Optional("name"): str,
-        vol.Required("states", default={}): dict
+        vol.Optional("cfg"): dict,
+        vol.Required("states", default={}): dict,
+        vol.Required("enabled", default=True): bool
     }, extra=vol.ALLOW_EXTRA)
 ])
+
+
+def yaml_entry_to_json(yaml_entry: dict) -> dict:
+    """Converts a yaml entry to a JSON entry"""
+    if "cfg" in yaml_entry:
+        cfg = yaml_entry["cfg"]
+    else:
+        cfg = yaml_entry.copy()
+        for key in ("id", "type", "name", "states", "enabled"):
+            cfg.pop(key, None)
+    return {
+        "type": yaml_entry["type"],
+        "name": yaml_entry.get("name", yaml_entry["id"]),
+        "unique_identifier": yaml_entry["id"],
+        "identifier": yaml_entry["id"],
+        "provider": "yaml",
+        "enabled": yaml_entry["enabled"],
+        "cfg": cfg,
+        "state_defaults": yaml_entry["states"],
+    }
 
 
 class ItemManager:
@@ -34,14 +55,47 @@ class ItemManager:
         self.core = core
         self.items = {}
         self.item_constructors = {}
+        self.storage = Storage(
+            self.core, "items", 1,
+            storage_init=lambda: {},
+            loader=self._load_items,
+            dumper=self._dump_items
+        )
+        self.item_config = self.storage.load_data()
 
     async def init(self) -> None:
         """Initialise the items from configuration"""
         self.yaml_cfg = await self.core.cfg.register_domain(
             "items", schema=CONFIG_SCHEMA)
+        self.load_yaml_config()
+
+        for storage_entry in self.item_config.values():
+            await self.create_from_storage_entry(storage_entry)
+
+    def load_yaml_config(self) -> None:
+        """Loads the YAML configuration"""
+        for key in tuple(self.item_config.keys()):
+            if self.item_config[key]["provider"] == "yaml":
+                del self.item_config[key]
 
         for yaml_entry in self.yaml_cfg:
-            await self.create_from_yaml_cfg(yaml_entry)
+            json_entry = yaml_entry_to_json(yaml_entry)
+            self.item_config[json_entry["unique_identifier"]] = json_entry
+        self.storage.schedule_save(self.item_config)
+
+    def update_storage_entry(self, entry: dict) -> None:
+        """Updates a config storage entry"""
+        self.item_config[entry["unique_identifier"]] = entry
+        self.storage.schedule_save(self.item_config)
+
+    def _load_items(self, data: dict) -> dict:
+        entries = {}
+        for entry in data:
+            entries[entry["unique_identifier"]] = entry
+        return entries
+
+    def _dump_items(self, data: dict) -> dict:
+        return list(data.values())
 
     async def add_from_module(self, mod_obj: Module) -> None:
         """
@@ -96,14 +150,14 @@ class ItemManager:
         self.core.event_engine.broadcast(EVENT_ITEM_REMOVED, item=item)
         LOGGER.info("Item %s has been removed", identifier)
 
-    async def create_from_yaml_cfg(self, yaml_entry: dict) -> Item:
-        """Creates an Item from a YAML entry"""
+    async def create_from_storage_entry(self, storage_entry: dict) -> Item:
+        """Creates an Item from a storage entry"""
         return await self.create_item(
-            identifier=yaml_entry["id"],
-            name=yaml_entry.get("name"),
-            item_type=yaml_entry["type"],
-            cfg=yaml_entry.get("cfg", yaml_entry),
-            state_defaults=yaml_entry["states"]
+            identifier=storage_entry["identifier"],
+            name=storage_entry["name"],
+            item_type=storage_entry["type"],
+            cfg=storage_entry["cfg"],
+            state_defaults=storage_entry["state_defaults"]
         )
 
     async def init_item(self, item: Item) -> None:
@@ -111,7 +165,7 @@ class ItemManager:
         LOGGER.debug("Initialising item %s", item.identifier)
         try:
             init_result = await item.init()
-        except Exception:  # pylint: disable=broad-except
+        except:  # pylint: disable=broad-except
             LOGGER.warning("An exception was raised when initialising item %s",
                            item.identifier,
                            exc_info=True)
@@ -134,23 +188,7 @@ class ItemManager:
             self, identifier: str, item_type: str,
             cfg: dict = None, state_defaults: dict = None, name: str = None
     ) -> Item:
-        """
-        Creates a HomeControl item
-
-        Args:
-            identifier (str):
-                The item identifier
-            item_type (str):
-                The type of your item consisting of MODULE.CLASS
-            cfg (dict):
-                The item's configuration
-            state_defaults (dict):
-                If the initial state cannot be polled on init
-                you can pass a default
-            name (str):
-                How your item should be displayed in the frontend
-
-        """
+        """Creates a HomeControl item"""
         if item_type not in self.item_constructors:
             LOGGER.error("Item type not found: %s", item_type)
             return

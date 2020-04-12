4e2c533a8b2402e9f2419b82dc58e9260a173cba
lennart-k <18233294+lennart-k@users.noreply.github.com>
2020-04-12 19:31:06 +0200

    :bug: Fixed IsItem validator

--- homecontrol/dependencies/validators.py
+++ homecontrol/dependencies/validators.py
@@ -13,11 +13,16 @@ class IsItem:
         self.msg = msg
 
     def __call__(self, identifier: str) -> "Item":
+        if not isinstance(identifier, str):
+            raise vol.Invalid(
+                f"Item identifier has to be string, not {type(identifier)}")
+
         item = self.core.item_manager.get_item(identifier)
         if not item:
             raise vol.Invalid(
                 self.msg.format(identifier=identifier)
-                or f"Item with identifier {identifier} does not exist")
+                if self.msg
+                else f"Item with identifier {identifier} does not exist")
         return item
 
     def __repr__(self):

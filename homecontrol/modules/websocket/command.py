6772c42b4016c1b949c4580dab8e2793d468a33d
lennart-k <18233294+lennart-k@users.noreply.github.com>
2020-05-04 11:55:08 +0200

    :sparkles: Added close callback WebSocketCommand

--- homecontrol/modules/websocket/command.py
+++ homecontrol/modules/websocket/command.py
@@ -1,7 +1,9 @@
 """The WebSocket command class"""
 from typing import TYPE_CHECKING
+import asyncio
 if TYPE_CHECKING:
     # pylint: disable=relative-beyond-top-level
+    from typing import Union
     from homecontrol.core import Core
     import voluptuous as vol
     from .message import WebSocketMessage
@@ -16,15 +18,26 @@ class WebSocketCommand:
     message: "WebSocketMessage"
     session: "WebSocketSession"
 
-    def __init__(
-            self, message: dict, core: "Core", session: "WebSocketSession"):
+    def __init__(self, message: "WebSocketMessage",
+                 core: "Core", session: "WebSocketSession") -> None:
         self.message = message
         self.core = core
         self.session = session
+        self.session.writer_task.add_done_callback(self._close)
 
     async def handle(self):
         """Handle a command"""
         raise NotImplementedError()
+
+    def _close(self, task: asyncio.Task):
+        asyncio.run_coroutine_threadsafe(self.close(), loop=self.core.loop)
+
+    async def close(self):
+        """Triggered when websocket is closing"""
+
+    def send_message(self, message: "Union[dict, str]") -> None:
+        """Sends a message"""
+        self.session.send_message(message)
 
     def success(self, data) -> dict:
         """Return a success response"""

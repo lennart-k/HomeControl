05a2b148787ac46be09b7b8e9910e64742552df7
lennart-k <18233294+lennart-k@users.noreply.github.com>
2020-05-15 22:49:06 +0200

    :recycle: Migrated Pushbullet from requests to aiohttp

--- homecontrol/modules/pushbullet.py
+++ homecontrol/modules/pushbullet.py
@@ -1,7 +1,9 @@
 """Pushbullet module"""
 
 import json
-import requests
+import logging
+import asyncio
+import aiohttp
 import voluptuous as vol
 
 from homecontrol.dependencies.entity_types import Item
@@ -10,6 +12,8 @@ from homecontrol.dependencies.action_engine import action
 SPEC = {
     "name": "Pushbullet"
 }
+
+LOGGER = logging.getLogger(__name__)
 
 BASE_URL = "https://api.pushbullet.com/v2"
 PUSH_URL = BASE_URL + "/pushes"
@@ -25,11 +29,16 @@ class Pushbullet(Item):
     async def init(self):
         """Initialise Pushbullet"""
         try:
-            return requests.get(ME_URL, headers={
-                "Access-Token": self.cfg["access_token"],
-                "Content-Type": "application/json"
-            }).status_code == 200
-        except requests.exceptions.ConnectionError:
+            async with aiohttp.ClientSession(
+                    loop=self.core.loop,
+                    timeout=aiohttp.ClientTimeout(total=2)) as session:
+                request = await session.get(ME_URL, headers={
+                    "Access-Token": self.cfg["access_token"],
+                    "Content-Type": "application/json"})
+                if not request.status == 200:
+                    return False
+        except asyncio.exceptions.TimeoutError:
+            LOGGER.error("Pushbullet API not reachable", exc_info=True)
             return False
 
     @action("send_message")
@@ -39,7 +48,8 @@ class Pushbullet(Item):
             "type": "note",
             **data
         }
-        requests.post(PUSH_URL, json.dumps(data), headers={
-            "Access-Token": self.cfg["access_token"],
-            "Content-Type": "application/json"
-        })
+        async with aiohttp.ClientSession(loop=self.core.loop) as session:
+            await session.post(PUSH_URL, data=json.dumps(data), headers={
+                "Access-Token": self.cfg["access_token"],
+                "Content-Type": "application/json"
+            })

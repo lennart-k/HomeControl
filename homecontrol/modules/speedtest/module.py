031ab1a5c7a8ebb6ce7f8b1fde0ca83c859ff5cf
Lennart K <18233294+lennart-k@users.noreply.github.com>
2020-06-28 12:46:17 +0200

    :sparkles: Adding a speedtest module

---
+++ homecontrol/modules/speedtest/module.py
@@ -0,0 +1,43 @@
+"""Module for internet speedtest"""
+
+import logging
+
+import speedtest
+
+from homecontrol.dependencies.action_engine import action
+from homecontrol.dependencies.entity_types import Item
+from homecontrol.dependencies.state_engine import StateDef
+
+LOGGER = logging.getLogger(__name__)
+
+
+class Speedtest(Item):
+    """The Speedtest item"""
+    download_speed = StateDef()
+    upload_speed = StateDef()
+    ping = StateDef()
+
+    async def init(self) -> None:
+        """Init the Speedtest item"""
+
+    @action("measure")
+    async def measure(self) -> None:
+        """
+        Measures the internetspeed
+        Gets called by state polling
+        """
+        LOGGER.debug("Doing speedtest")
+        s = speedtest.Speedtest()  # pylint: disable=invalid-name
+
+        def do_speedtest():
+            """Blocking task"""
+            s.get_best_server()
+            s.download()
+            s.upload(pre_allocate=False)
+
+        await self.core.loop.run_in_executor(None, do_speedtest)
+        await self.states.bulk_update(
+            ping=s.results.ping,
+            upload_speed=s.results.upload,
+            download_speed=s.results.download
+        )

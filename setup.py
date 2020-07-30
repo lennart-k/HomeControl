19684b0d5f09dcfd088a0cc6f3d251f7abfe945b
Lennart K <18233294+lennart-k@users.noreply.github.com>
2020-07-30 13:06:21 +0200

    Installing dev requirements with setup.py develop

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 """Setup script to install HomeControl as a package"""
 
 from setuptools import find_namespace_packages, setup
+from setuptools.command.develop import develop
 
 from homecontrol import const
 
@@ -14,7 +15,18 @@ except ModuleNotFoundError:
     pass
 
 REQUIREMENTS = open("requirements.txt").read().splitlines()
-MINIMUM_PYTHON_VERSION = ">="+".".join(map(str, const.MINIMUM_PYTHON_VERSION))
+REQUIREMENTS_DEV = open("requirements_dev.txt").read().splitlines()
+MINIMUM_PYTHON_VERSION = ">=" + \
+    ".".join(map(str, const.MINIMUM_PYTHON_VERSION))
+
+
+class DevelopCommand(develop):
+    """"Custom develop command that also install development requirements"""
+
+    def __init__(self, dist, **kw):
+        dist.install_requires.extend(REQUIREMENTS_DEV)
+        super().__init__(dist)
+
 
 setup(
     name="homecontrol",
@@ -27,6 +39,9 @@ setup(
     packages=find_namespace_packages(include=("homecontrol", "homecontrol.*")),
     include_package_data=True,
     install_requires=REQUIREMENTS + MODULE_REQUIREMENTS,
+    cmdclass={
+        "develop": DevelopCommand
+    },
     python_requires=MINIMUM_PYTHON_VERSION,
     package_data={
         "": ["*"]
